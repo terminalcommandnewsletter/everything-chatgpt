2a9bb171fcdf0a20a9c2d3bf2f7730d0c5e19dde
Terminal Command Newsletter <terminalcommandnewsletter@gmail.com>
2023-03-24 23:49:17 +0530

    Add more to client-side-js

--- client-side-js/264-13e92c51b0315184.js
+++ client-side-js/264-13e92c51b0315184.js
@@ -81,6 +81,46 @@ e.refreshApiKey = function(){
     if(this.apiKeyRefreshing)return this.apiKeyRefreshing;return this.apiKeyRefreshing=(0,o.Z)(function(){var e;return(0,h.__generator)(this,function(n){switch(n.label){case 0:return[4,(0,d.getSession)()];case 1:return(e=n.sent())&&this.setAccessToken(e.accessToken),[2];case 2:throw Error("Cannot refresh access token outside of browser");case 3:return[2]}})})
 }
 
+// OAuth callback for plugins
+e.pluginOauthCallback = function(e,code,redirect_uri,accessToken) {
+    var i=new URLSearchParams({code:code,redirect_uri:redirect_uri});
+    return this.fetch("".concat("https://chat.openai.com/backend-api","/aip/p/").concat(e,"/user-settings/oauth/callback?").concat(i),{method:"GET",headers:(0,s.Z)({"Content-Type":"application/json"},this.getAuthHeader(accessToken))})
+}
+
+// Get Page Metadata
+e.getPageMetadata = function(objWithUri){
+    var t = objWithUri.url;
+    return this.fetch("".concat("https://chat.openai.com/backend-api","/opengraph/tags?url=").concat(encodeURIComponent(t)),{method:"GET",headers:(0,s.Z)({"Content-Type":"application/json"},this.getAuthHeader())})
+}
+
+// Get Message Cap of Model (Turns out, the auth header isn't required, from what I can tell)
+e.getModelMessageCap = function(){
+    return this.fetch("https://chat.openai.com/public-api/conversation_limit",{method:"GET",headers:(0,s.Z)({"Content-Type":"application/json"},this.getAuthHeader())})
+}
+// As of March 24 6:08PM UTC, the returned response of the above endpoint is this:
+// message_cap: 25
+// message_cap_window: 180
+// message_disclaimer: 
+// textarea: "GPT-4 currently has a cap of 25 messages every 3 hours. Expect significantly lower caps, as we adjust for demand."
+// model-switcher: "You've reached the GPT-4 cap, which gives all ChatGPT Plus users a chance to try the model.\n\nPlease check back soon."
+
+// Delete Plugin
+e.deletePlugin = function(e){
+    var id = e.id,accessToken = e.accessToken;
+    return this.fetch("".concat("https://chat.openai.com/backend-api","/aip/p/").concat(id),{method:"DELETE",headers:(0,s.Z)({"Content-Type":"application/json"},this.getAuthHeader(accessToken))})
+}
+
+// Upload File (likely for plugins)
+e.upload = function(parentMessageId,conversationId,model,file){
+    var formData = new FormData;
+    return conversationId&&formData.append("conversation_id",conversationId),formData.append("model",model),formData.append("parent_message_id",parentMessageId),formData.append("file",file),this.fetch("".concat("https://chat.openai.com/backend-api","/conversation/upload"),{method:"POST",headers:(0,s.Z)({},this.getAuthHeader()),body:formData})
+}
+
+// Fetch File (likely for plugins)
+e.fetchFileForDownload = function(e,path){
+    var urlSearchParams = new URLSearchParams({path:path});return fetch("".concat("https://chat.openai.com/backend-api","/conversation/").concat(e,"/download?").concat(urlSearchParams),{method:"GET",headers:(0,s.Z)({},this.getAuthHeader())})
+}
+
 ///////// Unknown Purpose /////////////////
 // Create Artifacts
 e.createArtifact = function(url){
